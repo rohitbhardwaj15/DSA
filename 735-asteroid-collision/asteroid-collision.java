b1b671dd729e6f934a4cce8a73cde79e268744db
rohitbhardwaj15 <rohitbhardwaj1714@gmail.com>
2025-10-27 09:01:19 +0530

    Time: 6 ms (64.87%) | Memory: 45.4 MB (54.75%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
+
+        for (int a : asteroids) {
+            boolean destroyed = false;
+
+            while (!st.isEmpty() && a < 0 && st.peek() > 0) {
+                if (st.peek() < -a) {
+                    st.pop(); 
+                    continue;
+                } else if (st.peek() == -a) {
+                    st.pop(); 
+                }
+                destroyed = true; 
+                break;
+            }
+
+            if (!destroyed) {
+                st.push(a);
+            }
+        }
+
+        int[] res = new int[st.size()];
+        for (int i = st.size() - 1; i >= 0; i--) {
+            res[i] = st.pop();
+        }
+
+        return res;
+    }
+
+    
+}
