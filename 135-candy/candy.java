0d94ea01b365b3c171a75e620b42ba9f2ade288f
rohitbhardwaj15 <rohitbhardwaj1714@gmail.com>
2025-11-21 19:35:26 +0530

    Time: 3 ms (83.89%) | Memory: 47.4 MB (23.93%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -4,25 +4,24 @@ class Solution {
         if (n == 0) return 0;
         if (n == 1) return 1;
 
-        int total = 1;      // candies for the first child
-        int up = 0;         // length of current upward slope
-        int down = 0;       // length of current downward slope
-        int peak = 0;       // length of last upward slope (peak height)
+        int total = 1;      
+        int up = 0;        
+        int down = 0;      
+        int peak = 0;       
 
         for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {          // going up
+            if (ratings[i] > ratings[i - 1]) {          
                 up++;
                 peak = up;
                 down = 0;
-                total += 1 + up;                        // base 1 + extra for upward slope
-            } else if (ratings[i] == ratings[i - 1]) {   // flat
+                total += 1 + up;                        
+            } else if (ratings[i] == ratings[i - 1]) {   
                 up = down = peak = 0;
-                total += 1;                             // just 1 candy
-            } else {                                    // going down
+                total += 1;                       
+            } else {                                
                 down++;
                 up = 0;
-                // give child (1 + down) candies for descending part,
-                // but if peak >= down, we've already given enough to the peak so subtract 1
+              
                 total += 1 + down - (peak >= down ? 1 : 0);
             }
         }
