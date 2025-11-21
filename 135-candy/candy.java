0e4660b1530ba549de52a25ebb30558230cad3a9
rohitbhardwaj15 <rohitbhardwaj1714@gmail.com>
2025-11-21 19:32:46 +0530

    Time: 3 ms (83.89%) | Memory: 46.8 MB (29.49%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,30 +1,30 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int[] candies = new int[n];
+        if (n == 0) return 0;
+        if (n == 1) return 1;
 
-        
-        for(int i = 0; i < n; i++){
-            candies[i] = 1;
-        }
+        int total = 1;      // candies for the first child
+        int up = 0;         // length of current upward slope
+        int down = 0;       // length of current downward slope
+        int peak = 0;       // length of last upward slope (peak height)
 
-        
-        for(int i = 1; i < n; i++){
-            if(ratings[i] > ratings[i - 1]){
-                candies[i] = candies[i - 1] + 1;
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {          // going up
+                up++;
+                peak = up;
+                down = 0;
+                total += 1 + up;                        // base 1 + extra for upward slope
+            } else if (ratings[i] == ratings[i - 1]) {   // flat
+                up = down = peak = 0;
+                total += 1;                             // just 1 candy
+            } else {                                    // going down
+                down++;
+                up = 0;
+                // give child (1 + down) candies for descending part,
+                // but if peak >= down, we've already given enough to the peak so subtract 1
+                total += 1 + down - (peak >= down ? 1 : 0);
             }
-        }
-
-        for(int i = n - 2; i >= 0; i--){
-            if(ratings[i] > ratings[i + 1]){
-                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
-            }
-        }
-
-      
-        int total = 0;
-        for(int c : candies){
-            total += c;
         }
 
         return total;
