5aaea7d9ba953dc092d779a74262c394d608592e
rohitbhardwaj15 <rohitbhardwaj1714@gmail.com>
2026-03-03 02:16:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Square Root/square-root.java
@@ -0,0 +1,11 @@
+class Solution {
+    int floorSqrt(int n) {
+        // code here
+        int root = 0;
+        for(int i =0; i<=n;i++){
+            if(i*i>n) break;
+            root = i;
+        }
+        return root;
+    }
+}
