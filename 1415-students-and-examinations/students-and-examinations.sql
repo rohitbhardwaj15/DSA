52d071b8fd6e3229f6f97093b8dd63b357df0f5a
Rohit Bhardwaj <rohitbhardwaj1714@gmail.com>
2026-07-30 12:07:45 +0530

    Time: 1636 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,4 +1,4 @@
-SELECT 
+SELECT
     s.student_id,
     s.student_name,
     sub.subject_name,
@@ -6,12 +6,12 @@ SELECT
 FROM Students s
 CROSS JOIN Subjects sub
 LEFT JOIN Examinations e
-    ON s.student_id = e.student_id
-    AND sub.subject_name = e.subject_name
-GROUP BY 
-    s.student_id, 
-    s.student_name, 
+ON s.student_id = e.student_id
+AND sub.subject_name = e.subject_name
+GROUP BY
+    s.student_id,
+    s.student_name,
     sub.subject_name
-ORDER BY 
-    s.student_id, 
+ORDER BY
+    s.student_id,
     sub.subject_name;
